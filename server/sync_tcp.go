fead391145257773581ee8fc3cde0ac18a8dc8c3
aagarwal5 <aagarwal5@atlassian.com>
2022-10-22 16:23:01 +0530

    Committed ping

--- server/sync_tcp.go
+++ server/sync_tcp.go
@@ -1,31 +1,47 @@
 package server
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"strconv"
+	"strings"
 
 	"github.com/smartankur/dice/config"
+	"github.com/smartankur/dice/core"
 )
 
-func readCommand(c net.Conn) (string, error) {
+func readCommand(c net.Conn) (*core.RedisCmd, error) {
 	// TODO: Max read in one shot is 512 bytes
 	// To allow input > 512 bytes, then repeated read until
 	// we get EOF or designated delimiter
 	var buf []byte = make([]byte, 512)
 	n, err := c.Read(buf[:])
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(buf[:n]), nil
+
+	tokens, err := core.DecodeArrayString(buf[:n])
+	if err != nil {
+		return nil, err
+	}
+
+	return &core.RedisCmd{
+		Cmd:  strings.ToUpper(tokens[0]),
+		Args: tokens[1:],
+	}, nil
 }
 
-func respond(cmd string, c net.Conn) error {
-	if _, err := c.Write([]byte(cmd)); err != nil {
-		return err
+func respondError(err error, c net.Conn) {
+	c.Write([]byte(fmt.Sprintf("-%s\r\n", err)))
+}
+
+func respond(cmd *core.RedisCmd, c net.Conn) {
+	err := core.EvalAndRespond(cmd, c)
+	if err != nil {
+		respondError(err, c)
 	}
-	return nil
 }
 
 func RunSyncTCPServer() {
@@ -60,14 +76,9 @@ func RunSyncTCPServer() {
 				if err == io.EOF {
 					break
 				}
-				// as and when we try to terminate the connection then loop breaks
-				// then we are able to accept a new connection request
 				log.Println("err", err)
 			}
-			log.Println("command", cmd)
-			if err = respond(cmd, c); err != nil {
-				log.Print("err write:", err)
-			}
+			respond(cmd, c)
 		}
 	}
 }
