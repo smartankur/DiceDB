00cc2709d7d817532eaa9d7ae076e8d39aee3886
aagarwal5 <aagarwal5@atlassian.com>
2022-11-19 18:42:25 +0530

    added transaction

---
+++ server/sync_tcp.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"io"
+	"strings"
+
+	"github.com/smartankur/dice/core"
+)
+
+func toArrayString(ai []interface{}) ([]string, error) {
+	as := make([]string, len(ai))
+	for i := range ai {
+		as[i] = ai[i].(string)
+	}
+	return as, nil
+}
+
+func readCommands(c io.ReadWriter) (core.RedisCmds, error) {
+	// TODO: Max read in one shot is 512 bytes
+	// To allow input > 512 bytes, then repeated read until
+	// we get EOF or designated delimiter
+	var buf []byte = make([]byte, 512)
+	n, err := c.Read(buf[:])
+	if err != nil {
+		return nil, err
+	}
+
+	values, err := core.Decode(buf[:n])
+	if err != nil {
+		return nil, err
+	}
+
+	var cmds []*core.RedisCmd = make([]*core.RedisCmd, 0)
+	for _, value := range values {
+		tokens, err := toArrayString(value.([]interface{}))
+		if err != nil {
+			return nil, err
+		}
+		cmds = append(cmds, &core.RedisCmd{
+			Cmd:  strings.ToUpper(tokens[0]),
+			Args: tokens[1:],
+		})
+	}
+	return cmds, nil
+}
+
+func respond(cmds core.RedisCmds, c *core.Client) {
+	core.EvalAndRespond(cmds, c)
+}
